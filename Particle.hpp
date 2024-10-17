010edcb5449cc12606dc8c824c537fe09d1dd6fd
Mattia-mastro <mattia.mastroianni2@studio.unibo.it>
2024-10-17 13:01:48 +0200

    dario ilario
    aggiustato l'header di particle

--- Particle.hpp
+++ Particle.hpp
@@ -1,25 +1,20 @@
 #pragma once
-#include <string>
+#include "ResonanceType.hpp"
 #include <array>
+#include <string>
+#include <vector>
 
+class Particle : public ResonanceType
+{
+ private:
+  std::array<double, 3> impulse;
+  int index;
 
-class Particle : public ResonanceType {
-    std::array <double, 3> impulse;  
-    int index;
-<<<<<<< Updated upstream
-    static int findParticle (const std::string &name);
-    static std::array <ResonanceType *, 10> typeTable;
-    static void addParticleType(int name,int mass,int charge,double width)
-    Particle(const std::string &name, std::array <double, 3> impulse);
-=======
+  static std::vector<ResonanceType*> typeTable;
+  static int findParticle(const std::string& name);
 
-   static void addParticleType(ParticleType* type);
-   static int getNParticleType();
-   int getIndex() const;
-    double getPx() const;
-    double getPy() const;
-    double getPz() const;
-
-      
->>>>>>> Stashed changes
-}
+ public:
+  Particle(const std::string& name, std::array<double, 3> impulse = {0, 0, 0});
+  static void addParticleType(int name, int mass, int charge, double width = 0);
+  int getIndex() const;
+};
