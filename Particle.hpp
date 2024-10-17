64b9f5943e75cc8e21f2b598b7f9d21bbca1a70d
pratellipier <pierpaolo.pratelli@studio.unibo.it>
2024-10-17 12:39:30 +0200

    Update Particle.hpp

--- Particle.hpp
+++ Particle.hpp
@@ -6,8 +6,20 @@
 class Particle : public ResonanceType {
     std::array <double, 3> impulse;  
     int index;
+<<<<<<< Updated upstream
     static int findParticle (const std::string &name);
     static std::array <ResonanceType *, 10> typeTable;
     static void addParticleType(int name,int mass,int charge,double width)
     Particle(const std::string &name, std::array <double, 3> impulse);
+=======
+
+   static void addParticleType(ParticleType* type);
+   static int getNParticleType();
+   int getIndex() const;
+    double getPx() const;
+    double getPy() const;
+    double getPz() const;
+
+      
+>>>>>>> Stashed changes
 }
