725ce3381e62b80c0d95e646c11576248f906055
dsdesmond <167629040+dsdesmond@users.noreply.github.com>
2024-10-17 13:04:47 +0200

    kytf

--- Particle.cpp
+++ Particle.cpp
@@ -7,3 +7,19 @@ findParticle (std::string name)
     for (name =! typeTable[index].name; index ++)   
     return index;
 } 
+
+getNameArray (index) {
+    return typeTable[index].name
+}
+
+getMassArray (index) {
+    return typeTable[index].mass
+}
+
+getChargeArray (index) {
+    return typeTable[index].charge
+}
+
+getWidthArray (index) {
+    return typeTable[index].width
+}
--- Particle.hpp
+++ Particle.hpp
@@ -6,8 +6,20 @@
 class Particle : public ResonanceType {
     std::array <double, 3> impulse;  
     int index;
-    static int findParticle (const std::string &name);
     static std::array <ResonanceType *, 10> typeTable;
+    static int findParticle (const std::string &name);
     static void addParticleType(int name,int mass,int charge,double width)
     Particle(const std::string &name, std::array <double, 3> impulse);
+
+    auto getNameArray(int index);
+    auto getMassArray(int index);
+    auto getChargeArray(int index);
+    auto getWidthArray(int index);
+
+    auto getImpulse(int index);
+
 }
+
+
+
+
