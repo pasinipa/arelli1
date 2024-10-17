bba2d6192eff2db79492898aa1b92c8c80e395a9
dsdesmond <167629040+dsdesmond@users.noreply.github.com>
2024-10-17 12:34:01 +0200

    porcodio5

--- Particle.hpp
+++ Particle.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <string>
+#include <array>
 
 
-
-class Particle : public ResonceType {
+class Particle : public ResonanceType {
     std::array <double, 3> impulse;  
     int index;
     static int findParticle (const std::string &name);
     static std::array <ResonanceType *, 10> typeTable;
     static void addParticleType(int name,int mass,int charge,double width)
-
     Particle(const std::string &name, std::array <double, 3> impulse);
 }
