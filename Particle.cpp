56537855ff8439d66bb7f5b463a8e19fa430f278
dsdesmond <167629040+dsdesmond@users.noreply.github.com>
2024-10-17 12:31:28 +0200

    porcodio3

---
+++ Particle.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+
+findParticle (std::string name) 
+{
+    auto index = 0; 
+    for (name =! typeTable[index].name; index ++)   
+    return index;
+} 
--- Particle.hpp
+++ Particle.hpp
@@ -1,9 +1,14 @@
 #pragma once
+#include <string>
+
+
 
 class Particle : public ResonceType {
-    double impulse_X;
-    double impulse_Y;
-    double impulse_Z;  
+    std::array <double, 3> impulse;  
     int index;
-      
+    static int findParticle (const std::string &name);
+    static std::array <ResonanceType *, 10> typeTable;
+    static void addParticleType(int name,int mass,int charge,double width)
+
+    Particle(const std::string &name, std::array <double, 3> impulse);
 }
