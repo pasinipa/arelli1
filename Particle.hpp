5d59167cb849877c591ed77d39f8d57ae53f9eb3
Mattia-mastro <mattia.mastroianni2@studio.unibo.it>
2024-10-24 00:53:25 +0200

    aggiustato e finito l'header di particle

--- Particle.hpp
+++ Particle.hpp
@@ -1,42 +1,96 @@
 #pragma once
-#include "particle_type.hpp"
+#include "resonance_type.hpp"
 #include <array>
 #include <iostream>
-#include <map>
-#include <string>
 #include <vector>
 
-
 namespace p {
+struct Impulse
+{
+  double px_;
+  double py_;
+  double pz_;
+};
+
+inline double normImpulse(Impulse impulse)
+{
+  return std::sqrt(impulse.px_ * impulse.px_ + impulse.py_ * impulse.py_
+                   + impulse.pz_ * impulse.pz_);
+}
+
+inline Impulse sumVecImpulse(const Impulse& p1, const Impulse& p2)
+{
+  return Impulse{p1.px_ + p2.px_, p1.py_ + p2.py_, p1.pz_ + p2.pz_};
+}
+
 class Particle
 {
  private:
-  std::array<double, 3> impulse;
+  static const int maxNumParticleType{10};
+  static std::vector<pt::ParticleType*> fParticleType;
+  static int NParticleType;
   int index;
-  int energy;
+  std::array<double, 3> P;
 
-  static std::vector<ParticleType> typeTable;
-  static std::map<std::string, int> indexTable;
-    //auto getNameArray(int index);
-    //auto getMassArray(int index);
-    //auto getChargeArray(int index);
-    //auto getWidthArray(int index);
-    //auto getImpulse(int index);
+  static int findParticle(const std::string& name);
 
  public:
-  explicit Particle(const std::string& name,
-                    std::array<double, 3> impulse = {0., 0., 0.});
+  explicit Particle(const std::string&);
+  explicit Particle(const std::string&, Impulse);
 
-  static void addParticleType(const std::string& name,
-                              ParticleType particleType);
-  static void printTypeTable();
+  const int getIndex() const;
+  void setIndex(int);
+  void setIndex(const std::string&);
 
-  int getIndex() const;
-  void setIndex(int); //controllo cosa c'è alla posizione 3 e se voglio gli cambio l'index
-  void setIndex(const std::string&); //cerco se il nome è già presente e se voglio gli cambio l'index
-  //int calculateEnergy();
-  //int calculateInvariantMass();
-  //int findParticle(std::string name);
-  //void const printParticle(); 
+  const Impulse& getImpulse() const;
+  void setImpulse(Impulse);
+
+  static void addParticleType(const std::string&, double, int, double = 0);
+  static void printParticleType();
+
+  void Print() const;
+  const std::array<double, 3>& get_pulse() const;
 };
 } // namespace p
+
+// #include "particle_type.hpp"
+// #include <array>
+// #include <iostream>
+// #include <map>
+// #include <string>
+// #include <vector>
+
+// namespace p {
+// class Particle
+// {
+//  private:
+//   std::array<double, 3> impulse;
+//   int index;
+//   int energy;
+
+//   static std::vector<ParticleType> typeTable;
+//   static std::map<std::string, int> indexTable;
+//     //auto getNameArray(int index);
+//     //auto getMassArray(int index);
+//     //auto getChargeArray(int index);
+//     //auto getWidthArray(int index);
+//     //auto getImpulse(int index);
+
+//  public:
+//   explicit Particle(const std::string& name,
+//                     std::array<double, 3> impulse = {0., 0., 0.});
+
+//   static void addParticleType(const std::string& name,
+//                               ParticleType particleType);
+//   static void printTypeTable();
+
+//   int getIndex() const;
+//   void setIndex(int); //controllo cosa c'è alla posizione 3 e se voglio gli
+//   cambio l'index void setIndex(const std::string&); //cerco se il nome è già
+//   presente e se voglio gli cambio l'index
+//   //int calculateEnergy();
+//   //int calculateInvariantMass();
+//   //int findParticle(std::string name);
+//   //void const printParticle();
+// };
+// } // namespace p
