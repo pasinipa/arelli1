a36aadfeeed1d566c3d1c94bffbbb778839938e2
Mattia-mastro <mattia.mastroianni2@studio.unibo.it>
2024-10-24 11:53:08 +0200

    compilato in root

---
+++ resonance_type_cpp.d
@@ -0,0 +1,6 @@
+
+# DO NOT DELETE
+
+./resonance_type_cpp.so: resonance_type.hpp particle_type.hpp
+./resonance_type_cpp.so: /opt/homebrew/Cellar/root/6.32.06/include/root/RVersion.h /opt/homebrew/Cellar/root/6.32.06/include/root/ROOT/RConfig.hxx /opt/homebrew/Cellar/root/6.32.06/include/root/TClass.h /opt/homebrew/Cellar/root/6.32.06/include/root/TDictAttributeMap.h /opt/homebrew/Cellar/root/6.32.06/include/root/TInterpreter.h /opt/homebrew/Cellar/root/6.32.06/include/root/TROOT.h /opt/homebrew/Cellar/root/6.32.06/include/root/TBuffer.h /opt/homebrew/Cellar/root/6.32.06/include/root/TMemberInspector.h /opt/homebrew/Cellar/root/6.32.06/include/root/TError.h /opt/homebrew/Cellar/root/6.32.06/include/root/RtypesImp.h /opt/homebrew/Cellar/root/6.32.06/include/root/TIsAProxy.h /opt/homebrew/Cellar/root/6.32.06/include/root/TFileMergeInfo.h /opt/homebrew/Cellar/root/6.32.06/include/root/TCollectionProxyInfo.h /opt/homebrew/bin/rootcling
+resonance_type_cpp__ROOTBUILDVERSION= 6.32.06
