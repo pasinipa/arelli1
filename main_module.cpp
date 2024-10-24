0c464908fa7fe2df128a82535aff1c6e05986d1a
dsdesmond <167629040+dsdesmond@users.noreply.github.com>
2024-10-24 11:57:55 +0200

    dioporco 93

--- main_module.cpp
+++ main_module.cpp
@@ -7,13 +7,13 @@ int mainModule() {
     gRandom->SetSeed();
 
 
-    p::Particle::addParticleType:("Pione+",0.13957,+1);
-    p::Particle::addParticleType:("Pione-",0.13957,-1);
-    p::Particle::addParticleType:("Kaone+",0.49367,+1);
-    p::Particle::addParticleType:("Kaone-",0.49637,-1);
-    p::Particle::addParticleType:("Protone+",0.93827,+1);
-    p::Particle::addParticleType:("Protone-",0.93827,-1);
-    p::Particle::addParticleType:("K*",0.89166,0,0.050);
+    p::Particle::addParticleType("Pione+",0.13957,+1);
+    p::Particle::addParticleType("Pione-",0.13957,-1);
+    p::Particle::addParticleType("Kaone+",0.49367,+1);
+    p::Particle::addParticleType("Kaone-",0.49637,-1);
+    p::Particle::addParticleType("Protone+",0.93827,+1);
+    p::Particle::addParticleType("Protone-",0.93827,-1);
+    p::Particle::addParticleType("K*",0.89166,0,0.050);
 
 return 0;
 
