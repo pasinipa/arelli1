070ffda661444ba47adb060267492578ce0dace5
Mattia-mastro <mattia.mastroianni2@studio.unibo.it>
2024-10-24 10:53:34 +0200

    cacca

--- main_module.cpp
+++ main_module.cpp
@@ -1,20 +1,34 @@
-#include <iostream>
+#include "Particle.hpp"
 #include "particle_type.hpp"
 #include "resonance_type.hpp"
-#include "TRandom.h"
+#include <TRandom.h>
+#include <iostream>
 
-int main() {
-    gRandom->SetSeed();
+int main()
+{
+  gRandom->SetSeed();
 
+Particle:
+addParticleType:
+  ("Pione+", 0.13957, +1);
+Particle:
+addParticleType:
+  ("Pione-", 0.13957, -1);
+Particle:
+addParticleType:
+  ("Kaone+", 0.49367, +1);
+Particle:
+addParticleType:
+  ("Kaone-", 0.49637, -1);
+Particle:
+addParticleType:
+  ("Protone+", 0.93827, +1);
+Particle:
+addParticleType:
+  ("Protone-", 0.93827, -1);
+Particle:
+addParticleType:
+  ("K*", 0.89166, 0, 0.050);
 
-    Particle:addParticleType:("Pione+",0.13957,+1);
-    Particle:addParticleType:("Pione-",0.13957,-1);
-    Particle:addParticleType:("Kaone+",0.49367,+1);
-    Particle:addParticleType:("Kaone-",0.49637,-1);
-    Particle:addParticleType:("Protone+",0.93827,+1);
-    Particle:addParticleType:("Protone-",0.93827,-1);
-    Particle:addParticleType:("K*",0.89166,0,0.050);
-
-return 0;
-
+  return 0;
 }
