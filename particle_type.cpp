751863e139545ec17c741f43d6e54d8c2c676034
Mattia-mastro <mattia.mastroianni2@studio.unibo.it>
2024-10-24 11:40:00 +0200

    Revert "pirto"
    This reverts commit 4402fa912592ccf4fe3bd0276b98a172ade245cd.

--- particle_type.cpp
+++ particle_type.cpp
@@ -1,92 +1,33 @@
-// Do NOT change. Changes will be lost next time file is generated
-
-#define R__DICTIONARY_FILENAME particle_type
-#define R__NO_DEPRECATION
-
-/*******************************************************************/
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#define G__DICTIONARY
-#include "ROOT/RConfig.hxx"
-#include "TClass.h"
-#include "TDictAttributeMap.h"
-#include "TInterpreter.h"
-#include "TROOT.h"
-#include "TBuffer.h"
-#include "TMemberInspector.h"
-#include "TInterpreter.h"
-#include "TVirtualMutex.h"
-#include "TError.h"
-
-#ifndef G__ROOT
-#define G__ROOT
-#endif
-
-#include "RtypesImp.h"
-#include "TIsAProxy.h"
-#include "TFileMergeInfo.h"
-#include <algorithm>
-#include "TCollectionProxyInfo.h"
-/*******************************************************************/
-
-#include "TDataMember.h"
-
-// Header files passed as explicit arguments
 #include "particle_type.hpp"
+#include <iostream>
 
-// Header files passed via #pragma extra_include
+pt::ParticleType::ParticleType(const std::string& name, double mass, int charge)
+    : name{name}
+    , mass{mass}
+    , charge{charge}
+{}
 
-// The generated code does not explicitly qualify STL entities
-namespace std {} using namespace std;
-
-namespace {
-  void TriggerDictionaryInitialization_particle_type_Impl() {
-    static const char* headers[] = {
-"particle_type.hpp",
-nullptr
-    };
-    static const char* includePaths[] = {
-"/opt/homebrew/Cellar/root/6.32.06/include/root",
-"/Users/mattiamastroianni/Desktop/Università/Arelli/Arelli_1/",
-nullptr
-    };
-    static const char* fwdDeclCode = R"DICTFWDDCLS(
-#line 1 "particle_type dictionary forward declarations' payload"
-#pragma clang diagnostic ignored "-Wkeyword-compat"
-#pragma clang diagnostic ignored "-Wignored-attributes"
-#pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
-extern int __Cling_AutoLoading_Map;
-)DICTFWDDCLS";
-    static const char* payloadCode = R"DICTPAYLOAD(
-#line 1 "particle_type dictionary payload"
-
-
-#define _BACKWARD_BACKWARD_WARNING_H
-// Inline headers
-#include "particle_type.hpp"
-
-#undef  _BACKWARD_BACKWARD_WARNING_H
-)DICTPAYLOAD";
-    static const char* classesHeaders[] = {
-nullptr
-};
-    static bool isInitialized = false;
-    if (!isInitialized) {
-      TROOT::RegisterModule("particle_type",
-        headers, includePaths, payloadCode, fwdDeclCode,
-        TriggerDictionaryInitialization_particle_type_Impl, {}, classesHeaders, /*hasCxxModule*/false);
-      isInitialized = true;
-    }
-  }
-  static struct DictInit {
-    DictInit() {
-      TriggerDictionaryInitialization_particle_type_Impl();
-    }
-  } __TheDictionaryInitializer;
+const std::string& pt::ParticleType::getName() const
+{
+  return name;
 }
-void TriggerDictionaryInitialization_particle_type() {
-  TriggerDictionaryInitialization_particle_type_Impl();
+
+double pt::ParticleType::getMass() const
+{
+  return mass;
+}
+
+int pt::ParticleType::getCharge() const
+{
+  return charge;
+}
+
+void pt::ParticleType::print() const
+{
+  std::cout << "Name: " << name << '\n'
+            << std::endl
+            << "Mass: " << mass << "kg \n"
+            << std::endl
+            << "Charge: " << charge << " e \n"
+            << std::endl;
 }
