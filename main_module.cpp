0585e044f1cdb4495d402d81297bd70779e0d5b4
pratellipier <pierpaolo.pratelli@studio.unibo.it>
2024-10-24 10:23:16 +0200

    tipi di particelle
    ciao

---
+++ main_module.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "particle_type.hpp"
+#include "resonance_type.hpp"
+#include "TRandom.h"
+
+int main() {
+    gRandom->SetSeed();
+
+
+    Particle:addParticleType:("Pione+",0.13957,+1);
+    Particle:addParticleType:("Pione-",0.13957,-1);
+    Particle:addParticleType:("Kaone+",0.49367,+1);
+    Particle:addParticleType:("Kaone-",0.49637,-1);
+    Particle:addParticleType:("Protone+",0.93827,+1);
+    Particle:addParticleType:("Protone-",0.93827,-1);
+    Particle:addParticleType:("K*",0.89166,0,0.050);
+
+return 0;
+
+}
